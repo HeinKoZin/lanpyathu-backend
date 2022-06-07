7fad1df47c3f81b2e3eed8fde44e4b0c6fc4925d
Hein Ko Zin <heinkozin4@gmail.com>
2022-06-07 14:38:09 +0630

    ✨feature: Update User Role and password field is hide from query

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,8 +1,22 @@
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
+import {
+  InputType,
+  Field,
+  PartialType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { UserRole } from '@prisma/client';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
   @Field(() => String)
   id: string;
+
+  @Field(() => UserRole)
+  role?: UserRole;
 }
+
+registerEnumType(UserRole, {
+  name: 'UserRole',
+  description: 'User role',
+});
