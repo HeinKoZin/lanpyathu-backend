201db409f73babc85f851da02b91b14ad86586d4
Hein Ko Zin <heinkozin4@gmail.com>
2022-06-30 14:24:44 +0630

    ✨feature: current user decorator is added

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,9 +1,12 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LoginUserInput } from '@auth/dto/login.input';
 import { LoginResponse } from './dto/login.response';
 import { GqlAuthGuard } from './gql-auth.guard';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { CurrentUser } from '@decorators/current-user.decorator';
+import { CurrentUserEntity } from '@users/entities/current-user.entity';
 
 @Resolver()
 export class AuthResolver {
@@ -16,5 +19,11 @@ export class AuthResolver {
     @Context() context,
   ): Promise<LoginResponse> {
     return this.authService.login(context.user);
+  }
+
+  @Query(() => CurrentUserEntity)
+  @UseGuards(JwtAuthGuard)
+  async me(@CurrentUser() user: CurrentUserEntity): Promise<CurrentUserEntity> {
+    return user;
   }
 }
